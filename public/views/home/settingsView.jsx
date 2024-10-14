1d7ed4b754f582a4f507e075016eb3abfd286429
Daniel DeMoney <weston@MacBook-Air-6.local>
2024-10-14 14:12:48 -0700

    added settings ui, no function yet

--- public/views/home/settingsView.jsx
+++ public/views/home/settingsView.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { HomeViewNavBar } from './homeViewNavBar';
 import { LocalStorageHelper } from '../../../src/content/localStorageHelper';
+import '../../../src/assets/css/switch.css';
 
 export const SettingsView = () => {
     const [showingSignOutPopup, setShowingSignOutPopup] = useState(false);
@@ -33,13 +34,29 @@ export const SettingsView = () => {
             {/* end popup */}
             <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <p className='has-text-white is-size-3' style={{ textAlign: "center" }}>Settings:</p>
+                <div className='setting-row'>
+                    <p className='setting-text has-text-white is-size-4'>Auto-save every job: </p>
+                    <div className='setting-switch'>
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
+                    </div>
+                </div>
                 <button 
-                className='button is-large is-link mt-3'
+                className='button is-link mt-6'
                 onClick={()=>{
                     signOut();
                 }}
                 >
                     Sign Out
+                </button>
+                <button 
+                className='button is-danger mt-5'
+                onClick={()=>{
+                }}
+                >
+                    Delete Account
                 </button>
             </div>
         </div>
