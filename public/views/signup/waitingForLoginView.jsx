79f1c3de4dadd3fb33cbabf701a7dd30ca5111d2
Daniel DeMoney <weston@MacBook-Air-7.home.demoney.net>
2024-10-31 14:50:22 -0700

    forgot to add waitingForLoginView

---
+++ public/views/signup/waitingForLoginView.jsx
@@ -0,0 +1,44 @@
+import React, { createElement, useState } from 'react';
+
+export const WaitingForLoginView = ({showingPopup, setShowingPopup}) => {
+    return (
+        <div 
+        className={`popup ${showingPopup ? 'show' : ''} select-resume-popup p-4`}
+        >
+            <i 
+            className='fas fa-x' 
+            style={{
+                position: "absolute", 
+                left: "5%", 
+                top: "5%",
+                cursor: "pointer"
+            }}
+            onClick={()=>{
+                setShowingPopup(false);
+            }}></i>
+            <p className='has-text-white is-size-4'
+                style={{
+                    fontSize: "24px", 
+                    marginTop: "5%",
+                    width: "100%",
+                    textAlign: "center"
+                }}
+                >Waiting for login completion...</p>
+            <hr style={{width: "80%", marginBlockStart: "0.25em", marginBlockEnd: "0.25em"}}/>
+            <p className='mb-3'>Complete the login flow in the opened webpage</p>
+            <div className="spinner-container">
+                <div className="spinner" style={{width: "150px", height: "150px"}}></div>
+            </div>
+            <button 
+            className='button is-link mt-3' 
+            style={{width: "200px", textWrap: "wrap"}}
+            onClick={()=>{
+                const signupUrl = 'https://applicantiq.org/signup';
+                chrome.tabs.create({ url: signupUrl });
+            }}
+            >
+                Click here to reopen login window
+            </button>
+        </div>
+    )
+}
