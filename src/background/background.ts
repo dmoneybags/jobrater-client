9a4b52f2ef3b03e567d46bd822563d609383a661
Daniel DeMoney <weston@MacBook-Air-7.home.demoney.net>
2024-11-09 14:43:48 -0800

    added indeed support

--- src/background/background.ts
+++ src/background/background.ts
@@ -9,6 +9,8 @@ Sends: a message to contentscript to get the job
 */
 
 import { GlassdoorRequestingFunctions } from "../content/glassdoor";
+import { LinkedInFunctions } from "../content/linkedinFunctions";
+import { IndeedFunctions } from "../content/indeedFunctions";
 import { WindowingFunctions } from "./windowingFunctions";
 
 /*
@@ -38,9 +40,7 @@ commute time
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check for URL matches
-    if (tab.url && (tab.url.includes("linkedin.com/jobs/search") || 
-        tab.url.includes("linkedin.com/jobs/collections/") || 
-        tab.url.includes("linkedin.com/jobs/view/"))) {
+    if (tab.url && (LinkedInFunctions.urlMatchesLinkedIn(tab.url))) {
         console.log("GOT TAB CHANGE MESSAGE");
         //save a reference to the last tab that was activated
         chrome.storage.local.set({"currentTab": tabId});
@@ -70,6 +70,37 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                     company: "LINKEDIN",
                     //prefix for linkedIn jobs
                     jobId: "li" + jobId
+                }).then(response => {
+                    console.log('Message sent successfully:', response);
+                }).catch(error => {
+                    console.error('Error sending message to content script:', error);
+                });
+            } else {
+                console.error("No jobId found in the URL");
+            }
+        }
+    } else if (tab.url && (IndeedFunctions.urlMatchesIndeed(tab.url))){
+        console.log("GOT TAB CHANGE MESSAGE");
+        //save a reference to the last tab that was activated
+        chrome.storage.local.set({"currentTab": tabId});
+        console.log("SENDING CURRENT TABID TO " + tabId);
+        if (changeInfo.status === 'complete') {
+            let jobId = null;
+            const url = new URL(tab.url);  // Convert to URL object
+            const queryParams = new URLSearchParams(url.search); 
+            jobId = queryParams.get("vjk");
+            if (!jobId){
+                jobId = queryParams.get("jk");
+            }
+            if (jobId) {
+                console.log("Job ID found:", jobId);
+                // Send a message to contentScript to get pages and enter it in the DB
+                console.log("Sending message to content script");
+                chrome.tabs.sendMessage(tabId, {
+                    type: "NEW",
+                    company: "INDEED",
+                    //prefix for linkedIn jobs
+                    jobId: "in" + jobId
                 }).then(response => {
                     console.log('Message sent successfully:', response);
                 }).catch(error => {
